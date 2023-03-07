10fcccee9f690a76722c1dbcd54b586848692196
feisirony <>
2023-03-07 20:15:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+  Name: Jack Seeler & Blake Griffiths 
+  James Hargest College
+  Programming Internal for 2.7 & 2.8 ~ 12 credits
+  Due: 6 April 2023
+  
+  TIP: Use assessment guide to help guide you through this Internal
+'''
+
