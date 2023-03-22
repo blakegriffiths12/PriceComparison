7a2bdf0f9608aeecfbf0732c21ab2b8c06d3775a
feisirony <127250629+blakegriffiths12@users.noreply.github.com>
2023-03-22 23:03:25 +0000

    Added Comments in to split up comments

--- main.py
+++ main.py
@@ -6,24 +6,67 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+#---------------------------------imports---------------------------------------------
 
-# Exchange rates
-usd_rate = 1.0
-eur_rate = 1.2
-gbp_rate = 1.4
 
-# List of prices in USD
-prices_usd = [10.0, 20.0, 30.0, 40.0]
 
-# Function to convert prices to another currency
-def convert_prices(prices, rate):
-    return [price * rate for price in prices]
+#---------------------------------functions-------------------------------------------
+def int_check_answer(question,error):
+    valid=False 
+    while not valid:
+        try:
+          response=int(input(question))
+          return(response)
+        except ValueError:
+            print (error)
+# function to calculate price per gram
+def price_per_gram(price, weight):
+    return price / weight
 
-# Convert prices to EUR and GBP
-prices_eur = convert_prices(prices_usd, eur_rate)
-prices_gbp = convert_prices(prices_usd, gbp_rate)
 
-# Print original and converted prices
-print("Original prices (USD):", prices_usd)
-print("Prices converted to EUR:", prices_eur)
-print("Prices converted to GBP:", prices_gbp)
+
+
+def compare_prices(budget, products):
+    """Compare prices of products and recommend the best value for money"""
+    unit_prices = []
+    for product in products:
+        name, price, quantity = product
+        unit_price = price / quantity
+        unit_prices.append((name, unit_price))
+
+    # Sort the list of products by unit price
+    unit_prices.sort(key=lambda x: x[1])
+
+    # Find the product that fits within the budget and has the lowest unit price
+    affordable_products = [p for p in unit_prices if p[1] <= budget]
+    if not affordable_products:
+        return "Sorry, you can't afford any of these products."
+    else:
+        best_value = affordable_products[0][0]
+        return f"The best value for money is {best_value}."
+
+
+def get_product_info():
+    """Get product information from user input"""
+    name = input("Enter product name: ")
+    price = float(input("Enter product price: "))
+    quantity = float(input("Enter product quantity : "))
+    return (name, price, quantity)
+
+
+#------------------------------------------main---------------------------------------
+# Ask the user for their budget
+budget = float(input("Enter your budget: "))
+products = []
+while True:
+    add_product = input("Add a product? (y/n) ")
+    if add_product.lower() == "n":
+        break
+    else:
+        product_info = get_product_info()
+        products.append(product_info)
+
+# Call the compare_prices function and print the result
+result = compare_prices(budget, products)
+print(result)
+  
